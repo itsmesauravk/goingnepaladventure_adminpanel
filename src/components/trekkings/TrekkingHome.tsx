1c6b50a6525ded964497aa4791409e3a15ca3d58
Saurav Karki <sauravkarki10.12@gmail.com>
2024-11-16 22:39:08 +0545

    minor updated

--- src/components/trekkings/TrekkingHome.tsx
+++ src/components/trekkings/TrekkingHome.tsx
@@ -60,6 +60,39 @@ const TrekkingHome: React.FC = () => {
       {/* Separator */}
       <hr className="border-gray-300 mb-6" />
 
+      {/* Title */}
+      <h3 className="text-xl  text-gray-700 mb-4 font-semibold">
+        Our Trekkings
+      </h3>
+
+      {/* Filter and Search */}
+      <div className="flex items-center justify-between mb-6">
+        {/* Dropdown for Filter */}
+        <select
+          className="p-2 border border-gray-300 text-primary rounded-md w-1/3"
+          defaultValue=""
+        >
+          <option value="" disabled>
+            Filter by Category
+          </option>
+          <option value="mountain">Mountain</option>
+          <option value="desert">Desert</option>
+          <option value="forest">Forest</option>
+        </select>
+
+        {/* Search Input and Button */}
+        <div className="flex items-center space-x-2">
+          <input
+            type="text"
+            placeholder="Search Trekkings"
+            className="p-2 border text-primary  border-gray-300 rounded-md w-full"
+          />
+          <button className="bg-primary hover:bg-blue-700 text-white py-2 px-4 rounded-md">
+            Search
+          </button>
+        </div>
+      </div>
+
       {/* Trekking Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {trekking.map((trek) => (
@@ -98,7 +131,7 @@ const TrekkingHome: React.FC = () => {
             </p>
             {/* View Details Button */}
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md mt-4 w-full"
+              className="bg-primary hover:bg-secondary text-white py-2 px-4 rounded-md mt-4 w-full"
               onClick={() => router.push(`/trekking/${trek.slug}`)}
             >
               View Details
