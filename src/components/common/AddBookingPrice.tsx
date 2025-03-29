bfc403fc36efd3c3cf23ce5d7c5742ce37815fe4
Saurav Karki <sauravkarki10.12@gmail.com>
2025-03-29 14:42:07 +0545

    updated

--- src/components/common/AddBookingPrice.tsx
+++ src/components/common/AddBookingPrice.tsx
@@ -177,7 +177,7 @@ const AddBookingPrice: FC<AddBookingPriceProps> = ({
           <h3 className="text-xl font-semibold mb-4">
             Single Supplementary Prices
           </h3>
-          <div className="mb-4">
+          {/* <div className="mb-4">
             <label
               className="block text-gray-700 mb-2"
               htmlFor="singleSupplementary"
@@ -192,7 +192,7 @@ const AddBookingPrice: FC<AddBookingPriceProps> = ({
               onChange={handleChange}
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
-          </div>
+          </div> */}
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div className="mb-4">
--- src/components/common/EditBookingPrice.tsx
+++ src/components/common/EditBookingPrice.tsx
@@ -254,7 +254,7 @@ const UpdateBookingPrice: FC<UpdateBookingPriceProps> = ({
             <h3 className="text-xl font-semibold mb-4">
               Single Supplimentary Prices
             </h3>
-            <div className="mb-4">
+            {/* <div className="mb-4">
               <label
                 className="block text-gray-700 mb-2"
                 htmlFor="singleSupplementary"
@@ -269,7 +269,7 @@ const UpdateBookingPrice: FC<UpdateBookingPriceProps> = ({
                 onChange={handleChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-            </div>
+            </div> */}
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               <div className="mb-4">
